74cd07a103d97cb3b6144877785c2fb1ac5595b8
nyuristen <nyuristen@gmail.com>
2019-12-29 20:51:25 +0900

    完成した

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -39,7 +39,7 @@ $(function(){
         return html;
       };
     }
-$('.js-form').on('submit', function(e){
+$('.new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
