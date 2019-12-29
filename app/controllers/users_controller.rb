47bf7a10c0028c7c909d8b200cebf86b8a2718c0
nyuristen <nyuristen@gmail.com>
2019-12-30 00:54:09 +0900

    first_commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
+  def index
+    return nil if params[:keyword] == ""
+      @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
+      respond_to do |format|
+        format.html
+        format.json
+    end
+  end
   def edit
   end
-
   def update
     if current_user.update(user_params)
       redirect_to root_path
@@ -11,7 +18,6 @@ class UsersController < ApplicationController
   end
 
   private
-
   def user_params
     params.require(:user).permit(:name, :email)
   end
