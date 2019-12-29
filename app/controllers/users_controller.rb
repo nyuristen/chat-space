5c33d18fe15f8b56336708f6e54e5efef3dadc21
nyuristen <nyuristen@gmail.com>
2019-12-30 02:29:38 +0900

    インクリメンタルリサーチ完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,9 @@
 class UsersController < ApplicationController
   def index
-    return nil if params[:keyword] == ""
-      @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
-      respond_to do |format|
-        format.html
-        format.json
+    @users = User.search(params[:keyword], current_user.id)
+    respond_to do |format|
+      format.html
+      format.json
     end
   end
   def edit
